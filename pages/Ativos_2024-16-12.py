95cca984d43517028874545f984de1a94585929d
anatheresaa <anatecamartins@gmail.com>
2024-12-17 10:39:35 -0300

    Ativos_2024-12-16.py

--- pages/Ativos_2024-16-12.py
+++ pages/Ativos_2024-16-12.py
@@ -69,7 +69,7 @@ with tab_1:
                         
                 # Criar a métrica do total de avaliações feitas
                 with col_4:
-                        col_1.metric('Instituições de Ensino Citadas:', 63)
+                        col_1.metric('Instituições de Ensino Citadas:', 4)
                         
 # Preencher a tab 2 (Respostas Totais)
 with tab_2:

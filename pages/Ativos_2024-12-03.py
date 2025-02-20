fde45488c651e1625fdd899cf7f5d242da631ac9
anatheresaa <anatecamartins@gmail.com>
2025-02-20 11:43:40 -0300

    Ativos_2024-12-03.py

--- pages/Ativos_2024-12-03.py
+++ pages/Ativos_2024-12-03.py
@@ -45,7 +45,7 @@ with tab_1:
                         
                 # Criar a métrica do total de avaliações feitas
                 with col_4:
-                        col_1.metric('Instituições de Ensino Citadas:', 138)
+                        col_1.metric('Instituições de Ensino Citadas:', 125)
                         
         # Criar as métricas semenais
         with st.container():
@@ -66,7 +66,7 @@ with tab_1:
                         
                 # Criar a métrica do total de avaliações feitas
                 with col_4:
-                        col_1.metric('Instituições de Ensino Citadas:', 3)
+                        col_1.metric('Instituições de Ensino Citadas:', 2)
                         
 # Preencher a tab 2 (Respostas Totais)
 with tab_2:

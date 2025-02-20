cd567fd556d5cc974cbcbb89ffd1dee3f76dd480
anatheresaa <anatecamartins@gmail.com>
2025-02-20 11:35:24 -0300

    Aposentados_2024-11-15.py

--- pages/Aposentados_2024-11-15.py
+++ pages/Aposentados_2024-11-15.py
@@ -45,7 +45,7 @@ with tab_1:
                         
                 # Criar a métrica do total de avaliações feitas
                 with col_4:
-                        col_1.metric('Instituições de Ensino Citadas:', 50)
+                        col_1.metric('Instituições de Ensino Citadas:', 45)
 
         # Criar as métricas semenais
         with st.container():
@@ -66,7 +66,7 @@ with tab_1:
                         
                 # Criar a métrica do total de avaliações feitas
                 with col_4:
-                        col_1.metric('Instituições de Ensino Citadas:', 2)
+                        col_1.metric('Instituições de Ensino Citadas:', 1)
                         
 # Preencher a tab 2 (Respostas Totais)
 with tab_2:
